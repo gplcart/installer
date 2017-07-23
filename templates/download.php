358d7ecd059153614394cdb3d21c22b2a96619c8
GplCart <gplcart.software@gmail.com>
2017-07-23 20:06:18 +0300

    Update templates

--- templates/download.php
+++ templates/download.php
@@ -8,29 +8,25 @@
 ?>
 <form method="post" class="form-horizontal">
   <input type="hidden" name="token" value="<?php echo $_token; ?>">
-  <div class="panel panel-default">
-    <div class="panel-body">
-      <ul class="nav nav-tabs">
-        <?php if ($this->access('installer_module_upload')) { ?>
-        <li><a href="<?php echo $this->url('admin/module/install'); ?>"><?php echo $this->text('Upload'); ?></a></li>
-        <?php } ?>
-        <li class="active disabled"><a class="disabled"><?php echo $this->text('Download'); ?></a></li>
-      </ul>
-      <div class="tab-content">
-        <div class="tab-pane active">
-          <p><?php echo $this->text('<b>Warning!</b> You are fully responsible for content you provided! <b>Do not use untrusted sources</b> as they may distribute dangerous or invalid code that will damage all your site!'); ?></p>
-          <div class="form-group<?php echo $this->error('sources', ' has-error'); ?>">
-            <div class="col-md-12">
-              <textarea class="form-control" rows="10" name="download[sources]" placeholder="http://domain.com/module.zip"><?php echo isset($download['sources']) ? $this->e($download['sources']) : ''; ?></textarea>
-              <div class="help-block">
-                <?php echo $this->error('sources'); ?>
-                <div class="text-muted"><?php echo $this->text('One or more URLs to module zip files, one per line'); ?></div>
-              </div>
-            </div>
+  <ul class="nav nav-tabs">
+    <?php if ($this->access('installer_module_upload')) { ?>
+    <li><a href="<?php echo $this->url('admin/module/install'); ?>"><?php echo $this->text('Upload'); ?></a></li>
+    <?php } ?>
+    <li class="active disabled"><a class="disabled"><?php echo $this->text('Download'); ?></a></li>
+  </ul>
+  <div class="tab-content">
+    <div class="tab-pane active">
+      <p><?php echo $this->text('Warning! You are fully responsible for content you provided! <span class="text-danger">Do not use untrusted sources</span> as they may distribute dangerous or invalid code that will damage all your site!'); ?></p>
+      <div class="form-group<?php echo $this->error('sources', ' has-error'); ?>">
+        <div class="col-md-12">
+          <textarea class="form-control" rows="10" name="download[sources]" placeholder="http://domain.com/module.zip"><?php echo isset($download['sources']) ? $this->e($download['sources']) : ''; ?></textarea>
+          <div class="help-block">
+            <?php echo $this->error('sources'); ?>
+            <div class="text-muted"><?php echo $this->text('One or more URLs to module zip files, one per line'); ?></div>
           </div>
-          <button class="btn btn-default" name="install" value="1"><?php echo $this->text('Download'); ?></button>
         </div>
       </div>
+      <button class="btn btn-default" name="install" value="1"><?php echo $this->text('Download'); ?></button>
     </div>
   </div>
 </form>
--- templates/upload.php
+++ templates/upload.php
@@ -8,29 +8,25 @@
 ?>
 <form method="post" enctype="multipart/form-data" class="form-horizontal">
   <input type="hidden" name="token" value="<?php echo $_token; ?>">
-  <div class="panel panel-default">
-    <div class="panel-body">
-      <ul class="nav nav-tabs">
-        <li class="active disabled"><a class="disabled"><?php echo $this->text('Upload'); ?></a></li>
-        <?php if ($this->access('installer_module_download')) { ?>
-        <li><a href="<?php echo $this->url('admin/module/install/download'); ?>"><?php echo $this->text('Download'); ?></a></li>
-        <?php } ?>
-      </ul>
-      <div class="tab-content">
-        <div role="tabpanel" class="tab-pane active">
-          <p><?php echo $this->text('<b>Attention!</b> You are fully responsible for content you provided! <b>Do not use untrusted sources</b> as they may distribute dangerous or invalid code that will damage all your site!'); ?></p>
-          <div class="form-group<?php echo $this->error('file', ' has-error'); ?>">
-            <div class="col-md-4">
-              <input type="file" name="file" class="form-control">
-              <div class="help-block">
-                <?php echo $this->error('file'); ?>
-                <div class="text-muted"><?php echo $this->text('Select a ZIP file'); ?></div>
-              </div>
-            </div>
+  <ul class="nav nav-tabs">
+    <li class="active disabled"><a class="disabled"><?php echo $this->text('Upload'); ?></a></li>
+    <?php if ($this->access('installer_module_download')) { ?>
+    <li><a href="<?php echo $this->url('admin/module/install/download'); ?>"><?php echo $this->text('Download'); ?></a></li>
+    <?php } ?>
+  </ul>
+  <div class="tab-content">
+    <div role="tabpanel" class="tab-pane active">
+      <p><?php echo $this->text('Warning! You are fully responsible for content you provided! <span class="text-danger">Do not use untrusted sources</span> as they may distribute dangerous or invalid code that will damage all your site!'); ?></p>
+      <div class="form-group<?php echo $this->error('file', ' has-error'); ?>">
+        <div class="col-md-4">
+          <input type="file" name="file" class="form-control">
+          <div class="help-block">
+            <?php echo $this->error('file'); ?>
+            <div class="text-muted"><?php echo $this->text('Select a ZIP file'); ?></div>
           </div>
-          <button class="btn btn-default" name="install" value="1"><?php echo $this->text('Upload'); ?></button>
         </div>
       </div>
+      <button class="btn btn-default" name="install" value="1"><?php echo $this->text('Upload'); ?></button>
     </div>
   </div>
 </form>
